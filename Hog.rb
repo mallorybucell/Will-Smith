d164af7a0f3ce990e4859b6f87ecee6bd50e7a56
Mallory Bucell <m.f.bucell@gmail.com>
2015-01-30 12:38:23 -0500

    fixed storage of player name and scores into array nested in hash

--- Hog.rb
+++ Hog.rb
@@ -1,35 +1,19 @@
-#Enter multiple players
-  #Hash with Array as value for scores/names
+require 'minitest'
+require 'pry'
 
-
-
-
-#Game play
-max_score = 100
-toatl_scores = {}(0)
-
-puts "Welcome to Hog! Hog is a game played with 6-sided dice and at least 2 players."
-puts
-puts
-puts "First, who is playing? There must be at least two players."
-
-#def who_is_playing (Possibly should not be a function)
-  #Enter player name 
-  #Start Game or Add Player
-  #Player name => Key in Hash
-  #Check Hash has at least 2 keys
-  #Return Hash?
 
 
 #Ask Player how many dice to roll
+  #?? Check to make sure user put in a valid number.
 def player_turn
-  puts "It's _ turn. You can roll up to 20 6 dice. How many dice would you like to roll?"
+  puts "You can roll up to 20 6-sided dice. How many dice would you like to roll?"
   ndice = gets.chomp
-  return ndice
+  puts "You are rolling #{ndice} dice."
+  ndice.times do |roll|
+    roll = rand(1..6)
+    print roll, " "
+  end
 
-  puts "You are rolling #{ndice}. Hit "Enter" to roll."
-  #Check to make sure user put in a valid number.
-  #return ndice # of random values
   if #any die returns 1, 
     puts "Oh no! Your turn is over. You do not get any points." #Make sure this goes to player.next turn
     #turn score = 0
@@ -43,21 +27,53 @@ def player_turn
         puts "You scored {turn_score} for this turn."
       end
   end
+  return turn_score
 end
 
-def check_total_scores
-until #Some player_total_points = max_score
-  puts "Here is the current score:"
-  "Hit "n" to move on to the next player." #make this work
-  #n calls player_turn with next player in hash/order.
+
+
+namescore_pair = []
+allnames = []
+max_score = 100
+scoreboard = {}
+
+puts "Welcome to Hog! This version of the game is played with 6-sided dice and at least 2 players."
+puts
+puts
+puts "How many players?"
+players = gets.chomp.to_i
+until players == 0
+  puts "Please add a player name:"
+  playername = gets.chomp 
+  allnames.push(playername)
+  puts "You entered #{playername}."
+  players = players -= 1
 end
 
-puts "Good game! Player _ wins! Player __ is a Hog!"
+#Put player names and scores into Hash for scoreboard
+puts "Here is your list of players:"
 
-# ACII art
+allnames.each do |name|
+  namescore_pair.push([name,0])
+end 
+x = 0
+namescore_pair.each do |name|
+  x = x += 1
+  scoreboard["Player #{x}"] = name
+end
+puts scoreboard
+puts "Let's play!"
 
-#Do you want to play again? Y/N.  If Yes- restart program. If no, exit program.
+#Gameplay
+# until total_score ___ ||  ___ || ___ || >= max_score
+#   puts turn_score for #hash key in v[2]
+# puts "Next Up: scoreboard[loop total number of hash keys]""
+# end
 
 
+# puts "Good game! Player _ wins! Player __ is a Hog!"
 
+# # ACII art
+
+# #Do you want to play again? Y/N.  If Yes- restart program. If no, exit program.
 
